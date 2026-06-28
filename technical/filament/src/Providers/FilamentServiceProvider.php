054e443efe1405711214b8e12955790a53857536
Samoht70 <tpoirey70@gmail.com>
2026-06-28 19:59:53 +0200

    ✨ feat(filament): Scaffold technical/filament layer

---
+++ technical/filament/src/Providers/FilamentServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Technical\Filament\Providers;
+
+use Xefi\LaravelOSDD\LayerServiceProvider;
+
+class FilamentServiceProvider extends LayerServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+
+    public function register(): void
+    {
+        //
+    }
+}
